257a6e6a628b3d4353de58e6e29cbeb04bff45a8
Joshue Torres <josuetorresf2@gmail.com>
2023-06-15 10:28:10 -0500

    chat provider

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,17 @@
+import 'package:app_de_mensaje_joshue/domain/entities/message.dart';
+import 'package:flutter/material.dart';
+
+class ChatProvider extends ChangeNotifier {
+  /*
+    Puede notificar cuando hay cambios
+  */
+
+  List<Message> message = [
+    Message(text: 'Hola amor', fromWho: FromWho.mine),
+    Message(text: 'Ya regresaste? ', fromWho: FromWho.mine)
+  ];
+
+  Future<void> SendMessage(String text) async {
+    //TODO IMPLEMENTAR METODO
+  }
+}
